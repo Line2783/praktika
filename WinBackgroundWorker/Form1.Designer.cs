46c7ab5e9fde7b67e194b71107ac13e4e3e698ad
prakfksdfasf@outlook.com <prakfksdfasf@outlook.com>
2021-12-04 11:39:00 +0300

    перетащил backgroundworker и установил свойства

--- WinBackgroundWorker/Form1.Designer.cs
+++ WinBackgroundWorker/Form1.Designer.cs
@@ -35,6 +35,7 @@ namespace WinBackgroundWorker
             this.progressBar1 = new System.Windows.Forms.ProgressBar();
             this.button1 = new System.Windows.Forms.Button();
             this.button2 = new System.Windows.Forms.Button();
+            this.backgroundWorker1 = new System.ComponentModel.BackgroundWorker();
             this.SuspendLayout();
             // 
             // label1
@@ -88,6 +89,11 @@ namespace WinBackgroundWorker
             this.button2.Text = "button2";
             this.button2.UseVisualStyleBackColor = true;
             // 
+            // backgroundWorker1
+            // 
+            this.backgroundWorker1.WorkerReportsProgress = true;
+            this.backgroundWorker1.WorkerSupportsCancellation = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
@@ -114,6 +120,7 @@ namespace WinBackgroundWorker
         private System.Windows.Forms.ProgressBar progressBar1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Button button2;
+        private System.ComponentModel.BackgroundWorker backgroundWorker1;
     }
 }
 
