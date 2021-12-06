2ab557b6b4ff39dcb86dc70222cfdf926177d154
prakfksdfasf@outlook.com <prakfksdfasf@outlook.com>
2021-12-06 14:41:20 +0300

    Создал делегат

--- WinAsynchMethod/Form1.cs
+++ WinAsynchMethod/Form1.cs
@@ -11,10 +11,16 @@ using System.Windows.Forms;
 namespace WinAsynchMethod
 {
     public partial class Form1 : Form
+         private delegate int AsyncSumm(int a, int b);
     {
         public Form1()
         {
             InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
         }
     }
 }
