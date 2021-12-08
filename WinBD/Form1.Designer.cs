3c9873397fecdff89c5e7b7f371c9e237b4adf52
prakfksdfasf@outlook.com <prakfksdfasf@outlook.com>
2021-12-08 16:37:39 +0300

    Добавил кнопку,установил свойство

--- WinBD/Form1.Designer.cs
+++ WinBD/Form1.Designer.cs
@@ -33,6 +33,7 @@ namespace WinBD
             this.oleDbCommand1 = new System.Data.OleDb.OleDbCommand();
             this.button1 = new System.Windows.Forms.Button();
             this.listBox1 = new System.Windows.Forms.ListBox();
+            this.button2 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // oleDbConnection1
@@ -65,11 +66,21 @@ namespace WinBD
             this.listBox1.Size = new System.Drawing.Size(800, 95);
             this.listBox1.TabIndex = 1;
             // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(403, 24);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(233, 23);
+            this.button2.TabIndex = 2;
+            this.button2.Text = "Click here for Exercise 2";
+            this.button2.UseVisualStyleBackColor = true;
+            // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.button2);
             this.Controls.Add(this.listBox1);
             this.Controls.Add(this.button1);
             this.Name = "Form1";
@@ -84,6 +95,7 @@ namespace WinBD
         private System.Data.OleDb.OleDbCommand oleDbCommand1;
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.ListBox listBox1;
+        private System.Windows.Forms.Button button2;
     }
 }
 
