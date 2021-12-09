ad83452d43dc2ff7702009ce0b2c4301ca45a543
prakfksdfasf@outlook.com <prakfksdfasf@outlook.com>
2021-12-09 17:46:06 +0300

    Добавил 3 textbox,присвоил им нужные свойства,добвил 3 label,присвоил им нужные свойства!

--- WinBD/Form2.Designer.cs
+++ WinBD/Form2.Designer.cs
@@ -54,6 +54,12 @@ namespace WinBD
             this.телефонDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.фамилияDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
             this.городDataGridViewTextBoxColumn = new System.Windows.Forms.DataGridViewTextBoxColumn();
+            this.FamtextBox = new System.Windows.Forms.TextBox();
+            this.NametextBox = new System.Windows.Forms.TextBox();
+            this.SectiontextBox = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.rbProductDataSet1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.rbProductDataSet1BindingSource)).BeginInit();
@@ -122,7 +128,7 @@ namespace WinBD
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(12, 29);
+            this.button1.Location = new System.Drawing.Point(4, 24);
             this.button1.Name = "button1";
             this.button1.Size = new System.Drawing.Size(75, 23);
             this.button1.TabIndex = 2;
@@ -132,7 +138,7 @@ namespace WinBD
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(93, 29);
+            this.button2.Location = new System.Drawing.Point(4, 53);
             this.button2.Name = "button2";
             this.button2.Size = new System.Drawing.Size(124, 23);
             this.button2.TabIndex = 3;
@@ -143,7 +149,7 @@ namespace WinBD
             // label1
             // 
             this.label1.AutoSize = true;
-            this.label1.Location = new System.Drawing.Point(223, 34);
+            this.label1.Location = new System.Drawing.Point(163, 66);
             this.label1.Name = "label1";
             this.label1.Size = new System.Drawing.Size(67, 13);
             this.label1.TabIndex = 4;
@@ -152,7 +158,7 @@ namespace WinBD
             // label2
             // 
             this.label2.AutoSize = true;
-            this.label2.Location = new System.Drawing.Point(340, 34);
+            this.label2.Location = new System.Drawing.Point(127, 9);
             this.label2.Name = "label2";
             this.label2.Size = new System.Drawing.Size(71, 13);
             this.label2.TabIndex = 5;
@@ -160,7 +166,7 @@ namespace WinBD
             // 
             // SortTextBox
             // 
-            this.SortTextBox.Location = new System.Drawing.Point(226, 59);
+            this.SortTextBox.Location = new System.Drawing.Point(163, 85);
             this.SortTextBox.Name = "SortTextBox";
             this.SortTextBox.Size = new System.Drawing.Size(100, 20);
             this.SortTextBox.TabIndex = 6;
@@ -168,7 +174,7 @@ namespace WinBD
             // 
             // FilterTextBox
             // 
-            this.FilterTextBox.Location = new System.Drawing.Point(342, 59);
+            this.FilterTextBox.Location = new System.Drawing.Point(130, 27);
             this.FilterTextBox.Name = "FilterTextBox";
             this.FilterTextBox.Size = new System.Drawing.Size(100, 20);
             this.FilterTextBox.TabIndex = 7;
@@ -176,7 +182,7 @@ namespace WinBD
             // 
             // button3
             // 
-            this.button3.Location = new System.Drawing.Point(33, 70);
+            this.button3.Location = new System.Drawing.Point(4, 82);
             this.button3.Name = "button3";
             this.button3.Size = new System.Drawing.Size(153, 23);
             this.button3.TabIndex = 8;
@@ -254,11 +260,65 @@ namespace WinBD
             this.городDataGridViewTextBoxColumn.HeaderText = "Город";
             this.городDataGridViewTextBoxColumn.Name = "городDataGridViewTextBoxColumn";
             // 
+            // FamtextBox
+            // 
+            this.FamtextBox.Location = new System.Drawing.Point(354, 12);
+            this.FamtextBox.Name = "FamtextBox";
+            this.FamtextBox.Size = new System.Drawing.Size(100, 20);
+            this.FamtextBox.TabIndex = 10;
+            // 
+            // NametextBox
+            // 
+            this.NametextBox.Location = new System.Drawing.Point(354, 38);
+            this.NametextBox.Name = "NametextBox";
+            this.NametextBox.Size = new System.Drawing.Size(100, 20);
+            this.NametextBox.TabIndex = 11;
+            // 
+            // SectiontextBox
+            // 
+            this.SectiontextBox.Location = new System.Drawing.Point(354, 66);
+            this.SectiontextBox.Name = "SectiontextBox";
+            this.SectiontextBox.Size = new System.Drawing.Size(100, 20);
+            this.SectiontextBox.TabIndex = 12;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(280, 15);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(56, 13);
+            this.label3.TabIndex = 13;
+            this.label3.Text = "Фамилия";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(280, 41);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(29, 13);
+            this.label4.TabIndex = 14;
+            this.label4.Text = "Имя";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(280, 69);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(38, 13);
+            this.label5.TabIndex = 15;
+            this.label5.Text = "Отдел";
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(454, 261);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.SectiontextBox);
+            this.Controls.Add(this.NametextBox);
+            this.Controls.Add(this.FamtextBox);
             this.Controls.Add(this.dataGridView1);
             this.Controls.Add(this.button3);
             this.Controls.Add(this.FilterTextBox);
@@ -303,5 +363,11 @@ namespace WinBD
         private System.Windows.Forms.BindingSource rbProductDataSet1BindingSource;
         private RBProductDataSet rbProductDataSet1;
         private RBProductDataSetTableAdapters.ЗаказчикиTableAdapter заказчикиTableAdapter1;
+        private System.Windows.Forms.TextBox FamtextBox;
+        private System.Windows.Forms.TextBox NametextBox;
+        private System.Windows.Forms.TextBox SectiontextBox;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
     }
 }
